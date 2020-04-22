d569bce252f421430ec780e014b6a2284fde6168
Andrey Tkachuk <tka4uk.andrei@yandex.ru>
2020-04-22 23:38:36 +0300

    Add in part 2 lab 3

--- part_2/lab3_week_3.py
+++ part_2/lab3_week_3.py
@@ -1,17 +1,56 @@
+import os
+import urllib.request as r
+import time
+import queue
+from threading import Thread
 
-# Лаборатория
-# 1. Осуществить ввод адреса Интернет-страницы.
-# 2. Осуществить поиск всех изображений на странице.
-# 3. Удалить дубликаты.
-# 4. Удалить адреса без http или https.
-# 5. Вывести адреса изображений с нумерацией, нумерацию изображений начинать с 1.
-# 6. Потоками осуществить загрузку изображений и информацию о загрузке вывести на экран отдельными строками
-# 7. В случае невозможности загрузки выводить 'error'.
+image_links = []
+count = 0
 
-# Пример входных данных:
-# https://ya.ru/
+def download(q, cout):
+    tt = q.get(block=False)
+    try:
+        r.urlopen(tt)
+        print('Potok', str(cout), "zakonchil zagruzku", tt)
+    # print(self.name, g.get()
+    except:
+        print("error")
 
-# Пример выходных данных:
-# 1 izobrazhenie https://mc.yandex.ru/watch/723233
-# Potok 1 zakonchil zagruzku https://mc.yandex.ru/watch/723233
+http_adress = input()
+url_file = r.urlopen(http_adress)
 
+url_html = url_file.read().decode('utf-8')
+arr = url_html.split('<img')
+res = ''
+arr_dubl = []  # для проверки на дубликаты, также там будут хранится все подходящие ссыдки
+chech_doubl_bool = 1
+start_num = 1
+for i in range(1, len(arr), 1):
+    temp = arr[i].split('src="')
+    temp2 = temp[1].split('"')
+    for i in range(0, len(arr_dubl), 1):
+        if arr_dubl[i] == temp2[0]:
+            chech_doubl_bool = 0
+    start_str = temp2[0][0: -1 * (len(temp2[0])) + 5]
+    start_str2 = temp2[0][0: -1 * (len(temp2[0])) + 6]
+    if ((chech_doubl_bool == 1) and ((start_str == "http:") or (start_str2 == "https:"))):
+        arr_dubl.append(temp2[0])
+        print(str(start_num), "izobrazhenie", temp2[0])
+        start_num = start_num + 1
+    chech_doubl_bool = 1
+
+q = queue.Queue()
+for i in range(0, len(arr_dubl), 1):
+    q.put(arr_dubl[i])
+
+therads = []
+start = time.time()
+for i in range(0, len(arr_dubl), 1):
+    #download(q, i+1)
+    t = Thread(target=download, args=(q, i + 1))
+    t.setDaemon(True)
+    therads.append(t)
+    t.start()
+
+for i in therads:
+    i.join()
