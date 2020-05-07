4b5ff3c06233baa66b4f69bf9949e98a2d3b0852
Andrey Tkachuk <tka4uk.andrei@yandex.ru>
2020-05-08 02:49:21 +0300

    done lab6 in week 9

---
+++ part_2/week_9/django_venv/lab6_week_9/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Last_lab
+admin.site.register(Last_lab)
---
+++ part_2/week_9/django_venv/lab6_week_9/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Lab6Week9Config(AppConfig):
+    name = 'lab6_week_9'
