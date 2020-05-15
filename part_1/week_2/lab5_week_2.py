005c8a211c9bb241765834c75ea2cdda1cdf4d09
Andrey Tkachuk <tka4uk.andrei@yandex.ru>
2020-05-15 20:26:12 +0300

    fixed mismatch between lab 5 and 6 from part 1

--- part_1/week_2/lab5_week_2.py
+++ part_1/week_2/lab5_week_2.py
@@ -4,8 +4,4 @@ square = lambda a,b: (a * b)
 a = int(input())
 b = int(input())
 
-print(0, square (a, b))
-print(1, perim (a, b))
-print([square (a, b), perim (a, b)])
 print(square (a, b), perim (a, b))
-
